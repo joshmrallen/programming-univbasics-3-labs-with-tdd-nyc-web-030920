5bc708c1f6c4d7177ed59d623488d1cd848db047
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-22 05:18:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,10 @@
 # Add your variables here
 first_number = 8
 second_number = 4
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
 def find_sum(first_number, second_number)
   sum = first_number + second_number
