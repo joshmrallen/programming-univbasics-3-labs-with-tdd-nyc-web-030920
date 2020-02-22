2d88d933d5794839623036e3426ed6ed50d497ae
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-22 05:15:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,21 +2,25 @@
 
 def find_sum(first_number, second_number)
   sum = first_number + second_number
+  puts "The sum of #{first_number} and #{second_number} is #{sum}."
 end
 
 def find_difference(first_number, second_number)
   difference = first_number - second_number
+  puts "The difference of #{first_number} and #{second_number} is #{difference}."
 end
 
 def find_product(first_number, second_number)
   product = first_number*second_number
+  puts "The product of #{first_number} and #{second_number} is #{product}."
 end
 
 def find_quotient(first_number, second_number = 1)
   if second_number == 0
     second_number = 1 #set to default if 0 entered
-    puts "Cannot divded by zero"
+    puts "Cannot divded by zero."
   else 
     quotient = first_number / second_number
+    puts "The quotient of #{first_number} and #{second_number} is #{quotient}."
   end
 end
